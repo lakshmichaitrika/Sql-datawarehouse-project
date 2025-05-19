057d1718ce6b7bd8eba4af71c369df31c0f11d43
lakshmichaitrika <148346023+lakshmichaitrika@users.noreply.github.com>
2025-05-19 14:02:48 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,12 @@
+/*
+===================================================
+Create a database in pgadmin  with name 'Datawarehouse'
+=======================================================
+Open the query tool of  Datawarehouse 
+*/
+
+CREATE SCHEMA BRONZE
+
+CREATE SCHEMA Silver;
+
+CREATE SCHEMA Gold;
