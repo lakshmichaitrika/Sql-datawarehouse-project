083e75506065fd3e57bc0eb37e7afa2f111859c6
lakshmichaitrika <148346023+lakshmichaitrika@users.noreply.github.com>
2025-05-20 14:01:20 +0530

    Update bronze.sql

--- scripts/bronze.sql
+++ scripts/bronze.sql
@@ -10,12 +10,13 @@ with similar columns in the excelsheet
 CREATE TABLE IF NOT EXISTS bronze.crm_cust_info
 (
     cst_id integer,
-    cst_key character varying(50) COLLATE pg_catalog."default",
-    cst_firstname character varying(50) COLLATE pg_catalog."default",
-    cst_lastname character varying(50) COLLATE pg_catalog."default",
-    cst_material_status character varying(50) COLLATE pg_catalog."default",
-    cst_gndr character varying(50) COLLATE pg_catalog."default",
-    cst_create_date date
+    cst_key character varying(50),
+    cst_firstname character varying(50),
+    cst_lastname character varying(50),
+    cst_material_status character varying(50),
+    cst_gndr character varying(50) ,
+    cst_create_date date,
+	
 )
 
 CREATE TABLE  IF NOT EXISTS bronze.crm_prd_info(
@@ -26,42 +27,51 @@ prd_cost INT,
 prd_line VARCHAR(50),
 prd_start_dt DATE,
 prd_end_dt DATE,
-);
-CREATE TABLE  IF NOT EXISTS bronze.crm_prd_info(
-prd_id INT,
-prd_key VARCHAR(50),
-prd_nm VARCHAR(50),
-prd_cost INT,
-prd_line VARCHAR(50),
-prd_start_dt DATE,
-prd_end_dt DATE,
-);
+
+)
+
+CREATE TABLE IF NOT EXISTS bronze.crm_sales_details
+(
+    sls_ord_num character varying(50) COLLATE pg_catalog."default",
+    sls_prd_key character varying(50) COLLATE pg_catalog."default",
+    sls_cust_id integer,
+    sls_order_dt integer,
+    sls_ship_dt integer,
+    sls_due_dt integer,
+    sls_sales integer,
+    sls_quantity integer,
+    sls_price integer,
+	
+)
 
 CREATE TABLE  IF NOT EXISTS bronze.erp_cust_az12(
 cid VARCHAR(50),
 bdate DATE,
-gen VARCHAR(50)
+gen VARCHAR(50),
+
 );
 
 
 
 CREATE TABLE  IF NOT EXISTS bronze.erp_loc_a101(
 cid VARCHAR(50),
-cntry VARCHAR(50)
-);
+cntry VARCHAR(50),
 
-CREATE TABLE IF NOT EXISTS bronze.erp_cust_az12(
-cid VARCHAR(50),
-bdate DATE,
-gen VARCHAR(50)
 );
 
 
+CREATE TABLE IF NOT EXISTS bronze.erp_px_cat_g1v2
+(
+    id character varying(50) COLLATE pg_catalog."default",
+    cat character varying(50) COLLATE pg_catalog."default",
+    subcat character varying(50) COLLATE pg_catalog."default",
+    maintenance character varying(50) COLLATE pg_catalog."default",
 
-CREATE TABLE IF NOT EXISTS bronze.erp_loc_a101(
-cid VARCHAR(50),
-cntry VARCHAR(50)
-);  
+)
+
+
+
+ 
 
 
 /* After creating the tables =>
